fd294cb6f1d1d19e8fb1adbb86a533838d4a04f2
vivek gupta <131643827+Vivekgpta@users.noreply.github.com>
2025-09-09 15:59:15 +0530

    Update Video.jsx

--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -8,11 +8,12 @@ const Video = () => {
         autoPlay
         muted
         loop
-        src="https://download-video-ak.vimeocdn.com/v3-1/playback/a278bb9c-996c-4760-8ca1-dd0abff308f7/83c745cf?__token__=st=1756635268~exp=1756638868~acl=%2Fv3-1%2Fplayback%2Fa278bb9c-996c-4760-8ca1-dd0abff308f7%2F83c745cf%2A~hmac=9227d0d32a81eb4e22fe5842f10cd7bc383fc12e67f2fd7758e4ad0ed02af20a&r=dXMtd2VzdDE%3D"
+        src="https://download-video-ak.vimeocdn.com/v3-1/playback/a278bb9c-996c-4760-8ca1-dd0abff308f7/83c745cf?__token__=st=1757413637~exp=1757417237~acl=%2Fv3-1%2Fplayback%2Fa278bb9c-996c-4760-8ca1-dd0abff308f7%2F83c745cf%2A~hmac=32c9e14dd1b7eb721104fb04ae760f6b929caba846382ae1712b7dc1a558ec78&r=dXMtd2VzdDE%3D"
       ></video>
     </div>
   );
 };
 
 export default Video;
+
 
